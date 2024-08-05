f8e5714c04541f928371bf8c30b67c628130c22b
Sebastian <naesebastian96@gmail.com>
2024-08-05 16:17:21 +0300

    added shell design

--- frontend/messaging-app/src/app/feature-components/chatroom-shell/chatroom-shell.component.ts
+++ frontend/messaging-app/src/app/feature-components/chatroom-shell/chatroom-shell.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../../shared-components/header/header.component';
 
 @Component({
   selector: 'app-chatroom-shell',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent],
   templateUrl: './chatroom-shell.component.html',
   styleUrl: './chatroom-shell.component.scss'
 })
